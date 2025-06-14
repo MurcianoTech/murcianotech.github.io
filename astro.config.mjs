ec5c4e3092d0822317a9b0effa8e1777babd99c3
MurcianoTech <murcianotech@gmail.com>
2025-06-14 18:18:57 +0200

    [UPDATE] Update files for publish

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
     server: {
-      host: true, // Esto le dice a Astro que escuche en todas las interfaces de red, incluyendo tu IP local.
-      port: 3000, // Puedes mantener el puerto por defecto o cambiarlo si lo deseas
+      host: true,
+      port: 3000,
     },
-    // ... otras configuraciones ...
+    build: {
+      outDir: 'dist',
+      assets: 'assets',
+    },
 });
